b8d4ac712c74f2109c72d2a21df3009587ee9774
Akash Adhya <90973800+Akash-Adhya@users.noreply.github.com>
2026-04-16 23:28:53 +0530

    all responses changed from SimpleApiResponse to ApiResponse and the file is deleted

--- Backend/src/main/java/com/ems/service/employee/EmployeeRoomExitService.java
+++ Backend/src/main/java/com/ems/service/employee/EmployeeRoomExitService.java
@@ -1,8 +1,8 @@
 package com.ems.service.employee;
 
 import com.ems.dto.requestDto.RoomExitRequestDTO;
-import com.ems.dto.responsDto.SimpleApiResponse;
+import com.ems.dto.responsDto.ApiResponseDto;
 
 public interface EmployeeRoomExitService {
-    public SimpleApiResponse createExitRequest(Long employeeId, RoomExitRequestDTO dto);
+    public ApiResponseDto<String> createExitRequest(Long employeeId, RoomExitRequestDTO dto);
 }
