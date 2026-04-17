0908df3f369289294e1eaacd0a305a89ca033e38
Akash Adhya <90973800+Akash-Adhya@users.noreply.github.com>
2026-04-17 23:11:47 +0530

    All employee api endpoints and services are updated with security util class functions

--- Backend/src/main/java/com/ems/controllers/employee/EmployeeRoomController.java
+++ Backend/src/main/java/com/ems/controllers/employee/EmployeeRoomController.java
@@ -1,10 +1,19 @@
 package com.ems.controllers.employee;
 
+import com.ems.dto.responsDto.RoomResponseDTO;
+import com.ems.service.employee.EmployeeRoomService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/employee/room")
 public class EmployeeRoomController {
-    
+    EmployeeRoomService employeeRoomService;
+
+    @GetMapping
+    public ResponseEntity<RoomResponseDTO> getEmployeeRoom(){
+        return ResponseEntity.ok(employeeRoomService.getMyRoomDetails());
+    }
 }
